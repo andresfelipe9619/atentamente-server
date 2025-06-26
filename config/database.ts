06a49993889e625d0d8c5a08a1df540b33e298d4
Andrés Suárez | @suarezdev96 <andresfelipe9619@gmail.com>
2025-06-26 16:08:27 -0500

    ♻️ Update database pool configuration: increase `min` connections to 10 and `max` connections to 50

--- config/database.ts
+++ config/database.ts
@@ -35,7 +35,7 @@ export default ({ env }) => {
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: { min: env.int('DATABASE_POOL_MIN', 10), max: env.int('DATABASE_POOL_MAX', 50) },
     },
     sqlite: {
       connection: {
